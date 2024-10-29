7205c69b182484b3f17a3fab0a2cd7f5eba82a40
LuisSilva7 <lmpsilva7@gmail.com>
2024-10-29 01:25:06 +0000

    Implemented PopularProducts logic from backend

--- src/components/homePage/PopularProducts.jsx
+++ src/components/homePage/PopularProducts.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./popularProducts.module.css";
-import movies from "../../../movies.json";
 import { Link } from "react-router-dom";
 
 const PopularProducts = () => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/movies/popular",
+          {
+            method: "GET",
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Error fetching movies.");
+        }
+
+        const data = await response.json();
+        setMovies(data.data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
     <div className={styles["popular-products-wrapper"]}>
       <div className={styles["popular-section"]}>
         <h2 className={styles["popular-section-text"]}>Popular Movies</h2>
       </div>
+      {error && <p>{error}</p>}
       <div className={styles["popular-products-grid"]}>
-        {movies.slice(0, 12).map((movie) => (
-          <div key={movie.id} className={styles["popular-producs-grid-item"]}>
+        {movies.map((movie) => (
+          <div key={movie.id} className={styles["popular-products-grid-item"]}>
             <Link to={`/products/${movie.id}`}>
               <img
-                src={`../../../movies-thumb/${movie.image}`}
+                src={`../../../movies-thumb/${movie.image}`} // Certifique-se de que o caminho da imagem está correto
                 alt={movie.name}
               />
             </Link>
