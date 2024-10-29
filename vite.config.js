35f18dbcf9d70b4e6e4e1101fe9da2bdc8b3c0d6
LuisSilva7 <lmpsilva7@gmail.com>
2024-10-29 00:16:26 +0000

    env test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BACKEND_URL || "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
