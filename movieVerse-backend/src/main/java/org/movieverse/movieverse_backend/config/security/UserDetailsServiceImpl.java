9177939face280cf1aaa120603a66e667af78316
LuisSilva7 <lmpsilva7@gmail.com>
2024-12-15 04:18:13 +0000

    implemented security configs

---
+++ movieVerse-backend/src/main/java/org/movieverse/movieverse_backend/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package org.movieverse.movieverse_backend.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.movieverse.movieverse_backend.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username: " + username + " not found!"));
+    }
+}
---
+++ movieVerse-backend/src/main/java/org/movieverse/movieverse_backend/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package org.movieverse.movieverse_backend.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role);
+}
